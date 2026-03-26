b8af3156029fa4370f740aab2f59945b147e0c91
EDWINCSN <c.edwin32@hotmail.com>
2026-03-25 19:29:32 -0500

    feat: add constructores in class Cliente

--- Cliente.java
+++ Cliente.java
@@ -7,6 +7,19 @@ public class Cliente {
     private int totalCompras; //contador de cuantas compras ha realizado el cliente
 
 
+    //CONSTRUCTOR VACIO
+    public Cliente() {
+        
+    }
 
+    //CONSRUCTOR CON PARAMETROS
+    public Cliente(String nombre, String correo, double saldo) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.saldo = saldo;
+        this.totalCompras = 0;
+    }
     
+
+
 }
