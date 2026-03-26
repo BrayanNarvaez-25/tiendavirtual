29ea24d905d8222f4a5419eaf59cea5c0cc66ce4
BrayanNarvaez-25 <brayann251107g@gmail.com>
2026-03-25 20:18:34 -0500

    tiendavirtual - v1.0 (#5)
    * feat: create class Main.java
    
    * tiendavirtual - v1.0 (#4)
    
    * Feature/brayan narváez integrante1 (#1)
    
    * feat: create file Producto.java
    
    * feat: create constructor and attributes of de class Producto.java
    
    * feat: create method mostrarInfo
    
    * feat: create method aplicarDescuento
    
    * feat: create method verificarStock
    
    * Feature/angelmorales integrante3 (#3)
    
    * feat: create class Carrito.java
    
    * feat: create contructor and attributes
    
    * feat: create metod agregarProducto
    
    * feat: create metod calcularTotal
    
    * feat: create metod mostrarResumen
    
    * feat: correct error
    
    * feat: create metod mostrarResumen
    
    * feat: create Total
    
    * feat: correcction
    
    * Merge pull request #2 from BrayanNarvaez-25/feature/edwincusin-integrante2
    
    Feature/edwincusin integrante2
    
    ---------
    
    Co-authored-by: angel-hub31 <moralesmales1@hotmail.com>
    Co-authored-by: EDWINCSN <30878024+edwincusin@users.noreply.github.com>
    
    * refactor: delete package of the class Cliente.java and refactoring contructor of Producto.java in class Main.java
    
    ---------
    
    Co-authored-by: angel-hub31 <moralesmales1@hotmail.com>
    Co-authored-by: EDWINCSN <30878024+edwincusin@users.noreply.github.com>

---
+++ Cliente.java
@@ -0,0 +1,78 @@
+public class Cliente {
+    private String nombre; //nombre completo cliente
+    private String correo; //correo electronico cliente
+    private double saldo; //Dinero disponible de la cuenta del cliente
+    private int totalCompras; //contador de cuantas compras ha realizado el cliente
+
+
+    //CONSTRUCTOR VACIO
+    public Cliente() {
+        
+    }
+
+    //CONSRUCTOR CON PARAMETROS
+    public Cliente(String nombre, String correo, double saldo) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.saldo = saldo;
+        this.totalCompras = 0;
+    }
+
+    
+
+    //METODOS GETTERS Y SETTERS
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public int getTotalCompras() {
+        return totalCompras;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    public void setTotalCompras(int totalCompras) {
+        this.totalCompras = totalCompras;
+    }
+
+    //METODO MOSTRAR PERFIL
+    public void mostrarPerfil() {
+        System.out.println("Cliente='" + nombre + '\'' +
+                ", Correo='" + correo + '\'' +
+                ", Saldo=" + saldo +
+                ", Compras realizadas=" + totalCompras);
+    }
+        //METODO REALIZAR COMPRA
+    public void realizarCompra(double monto) {
+        if (monto > saldo) {
+            System.out.println("Saldo insuficiente.");
+            System.out.println("Saldo actual " + saldo);
+        } else {
+            saldo -= monto;
+            totalCompras++;
+            System.out.println("Compra exitosa por " + monto);
+            System.out.println("Saldo restante: " + saldo);
+        }
+    }
+
+    //METODO MOSTRAR SALDO
+    public void mostrarSaldo() {
+        System.out.println("Saldo actual: " + saldo);
+    }
+
+
+}
---
+++ Main.java
@@ -0,0 +1,45 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("===== Bienvenido a la Tienda Digital =====");
+        System.out.println();
+
+        // --- Integrante 1: Producto ---
+        // Crear una instancia de Producto con datos reales.
+        Producto producto1 = new Producto("Audífonos Bluetooth","Electrónica", 89.99, 50 );
+        
+        // Llamar a: mostrarInfo(), aplicarDescuento() y verificarStock().
+        producto1.mostrarInfo();
+        producto1.aplicarDescuento(15.0);
+        producto1.verificarStock(10);
+        System.out.println();
+
+
+        // --- Integrante 2: Cliente ---
+        // Crear una instancia de Cliente con datos reales.
+        Cliente cliente1 = new Cliente("María López", "maria@email.com", 250.00);
+        
+        // Llamar a: mostrarPerfil(), realizarCompra() y mostrarSaldo().
+        cliente1.mostrarPerfil();
+        cliente1.realizarCompra(89.99);
+        cliente1.mostrarSaldo();
+        System.out.println();
+
+
+        // --- Integrante 3: Carrito ---
+        // Crear una instancia de Carrito.
+        Carrito carrito1 = new Carrito("María López");
+        
+        // Agregar al menos 2 productos con agregarProducto().
+        carrito1.agregarProducto("Audífonos Bluetooth", 89.99);
+        carrito1.agregarProducto("Cargador USB", 15.00);
+        
+        // Llamar a calcularTotal() y mostrarResumen().
+        carrito1.calcularTotal();
+        carrito1.mostrarResumen();
+
+
+        System.out.println();
+        System.out.println("===== Fin del programa =====");
+    }
+}
