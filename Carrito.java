651fe27d57aed7b10ada4735637811b6795e3986
angel-hub31 <moralesmales1@hotmail.com>
2026-03-25 19:38:29 -0500

    feat: create metod mostrarResumen

--- Carrito.java
+++ Carrito.java
@@ -35,6 +35,7 @@ public class Carrito {
         for (int i = 0; i < cantidad; i++) {
             System.out.println((i + 1) + ". " + productos[i] + " $" + precios[i]);
         }
+        
 
     
 }
